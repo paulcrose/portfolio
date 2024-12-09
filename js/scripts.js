54e553770bac2fd01916ad1bc8878b622112a0d5
paulcrose <61593103+paulcrose@users.noreply.github.com>
2024-12-09 16:44:09 -0500

    x
    x

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 /*!
-* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Viewport Height:", window.innerHeight);
+  console.log("Viewport Width:", window.innerWidth);
+});
